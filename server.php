ae5a71cdd51b0f812cd447925f003474c854ea4a
Gokhan Baris Aker <gokhanbarisaker@gmail.com>
2015-06-09 16:39:49 +0400

    Fixed header setter usage

--- server.php
+++ server.php
@@ -31,7 +31,7 @@ function routeRequest()
         }
         header('Content-Type: application/json');
         header('Cache-Control: no-cache');
-        header('Access-Control-Allow-Origin', '*');
+        header('Access-Control-Allow-Origin: *');
         echo $comments;
     } else {
         return false;
