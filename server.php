1e006e4e340d740ae73334bf70fd4395a0592568
Gokhan Baris Aker <gokhanbarisaker@gmail.com>
2015-06-09 16:10:44 +0400

    Enabled CORS requests for php

--- server.php
+++ server.php
@@ -31,9 +31,9 @@ function routeRequest()
         }
         header('Content-Type: application/json');
         header('Cache-Control: no-cache');
+        header('Access-Control-Allow-Origin', '*');
         echo $comments;
     } else {
         return false;
     }
 }
-
